70e5d583326965f1d85e4d5413b0f719d449d8ec
kh3996 <kh3996@gmail.com>
2020-02-12 23:48:52 -0400

    initial design for dashboard page

--- client/app/app-routing.module.ts
+++ client/app/app-routing.module.ts
@@ -1,8 +1,37 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { 
+    path: '', 
+    redirectTo: 'home', 
+    pathMatch: 'full' 
+  },
+  { 
+    path: 'home', 
+    component: HomeComponent
+  },
+  // {
+  //   path: 'customers',
+  //   loadChildren: './customers/customers.module#CustomersModule'
+  // },
+  // {
+  //   path: 'orders',
+  //   loadChildren: './orders/orders.module#OrdersModule'
+  // },
+  // {
+  //   path: 'messages',
+  //   loadChildren: './messages/messages.module#MessagesModule'
+  // },
+  // {
+  //   path: '',
+  //   redirectTo: '',
+  //   pathMatch: 'full'
+  // }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -6,12 +6,31 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 // Material
 import { MaterialModule } from './material/material.module';
 
+// Routing
 import { AppRoutingModule } from './app-routing.module';
+
+// Component
 import { AppComponent } from './app.component';
+import { SidevarCardComponent } from './sidevar/sidevar-card/sidevar-card.component';
+import { UserButtonComponent } from './sidevar/user-button/user-button.component';
+import { AdminButtonComponent } from './sidevar/admin-button/admin-button.component';
+import { IndividualButtonComponent } from './sidevar/individual-button/individual-button.component';
+import { HomeComponent } from './home/home.component';
+import { HomeInputComponent } from './home/home-input/home-input.component';
+import { HomeFeedComponent } from './home/home-feed/home-feed.component';
+import { ToolbarComponent } from './sidevar/toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidevarCardComponent,
+    UserButtonComponent,
+    AdminButtonComponent,
+    IndividualButtonComponent,
+    HomeComponent,
+    HomeInputComponent,
+    HomeFeedComponent,
+    ToolbarComponent,
   ],
   imports: [
     BrowserModule,
