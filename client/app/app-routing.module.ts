241803501a7a18ca2f4e6be1c8e02755491c5bc8
kh3996 <kh3996@gmail.com>
2020-03-06 12:00:23 -0400

    before edit routing

--- client/app/app-routing.module.ts
+++ client/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-<<<<<<< HEAD
 import { HomeComponent } from './components/home/home.component';
 import { WageOverviewComponent } from './components/wage/wage-overview/wage-overview.component';
 import { SigninComponent } from './components/signin/signin.component';
@@ -11,22 +10,10 @@ import { NewUserComponent } from './components/main-nav/sidevar/admin-button/new
 import { NoteComponent } from './components/main-nav/sidevar/user-button/note/note.component';
 import { CalendarComponent } from './components/main-nav/sidevar/user-button/calendar/calendar.component';
 import { CalendarNewScheduleComponent } from './components/main-nav/sidevar/user-button/calendar/calendar-new-schedule/calendar-new-schedule.component';
-import { PermissionDetailComponent } from './components/main-nav/sidevar/admin-button/permission/permission-detail/permission-detail.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
-=======
-import { HomeComponent } from "./components/home/home.component";
-import { WageOverviewComponent } from "./components/wage/wage-overview/wage-overview.component";
-import { SigninComponent } from "./components/signin/signin.component";
-import { RegisterComponent } from "./components/register/register.component";
-import { StaffComponent } from "./components/user-menus/staff/staff.component";
-import { NewUserComponent } from "./components/main-nav/sidevar/admin-button/new-user/new-user.component";
-import { NoteComponent } from "./components/main-nav/sidevar/user-button/note/note.component";
-import { CalendarComponent } from "./components/main-nav/sidevar/user-button/calendar/calendar.component";
-import { CalendarNewScheduleComponent } from "./components/main-nav/sidevar/user-button/calendar/calendar-new-schedule/calendar-new-schedule.component";
-import { PermissionDetailComponent } from "./components/main-nav/sidevar/admin-button/permission/permission-detail/permission-detail.component";
+import { PermissionDetailComponent } from './components/main-nav/sidevar/admin-button/permission/permission-detail/permission-detail.component';
 import { MyInfoComponent } from './components/user-menus/my-info/my-info.component';
 import { WageListOfStaffComponent } from './components/user-menus/wage-list-of-staff/wage-list-of-staff.component';
->>>>>>> 9409547350ca032776359d2557b6f856beddf852
 
 const routes: Routes = [
   {
@@ -79,15 +66,11 @@ const routes: Routes = [
     component: PermissionDetailComponent
   },
   {
-    path: 'wage-overview',
-    component: WageOverviewComponent
-  },
-  {
-    path: "wagelist",
+    path: 'wagelist',
     component: WageListOfStaffComponent
   },
-    {
-    path: "myinfo",
+  {
+    path: 'myinfo',
     component: MyInfoComponent
   },
 
--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -1,57 +1,57 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { LayoutModule } from "@angular/cdk/layout";
-import { FlexLayoutModule } from "@angular/flex-layout";
-import { HttpClientModule } from "@angular/common/http";
-import { ChartsModule } from "ng2-charts";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@angular/cdk/layout';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { HttpClientModule } from '@angular/common/http';
+import { ChartsModule } from 'ng2-charts';
 
 // Material
-import { MaterialModule } from "./material/material.module";
+import { MaterialModule } from './material/material.module';
 
 // Routing
-import { AppRoutingModule } from "./app-routing.module";
+import { AppRoutingModule } from './app-routing.module';
 
 // Component
-import { AppComponent } from "./app.component";
-import { MainNavComponent } from "./components/main-nav/main-nav.component";
-import { SidevarCardComponent } from "./components/main-nav/sidevar/sidevar-card/sidevar-card.component";
-import { WageOverviewComponent } from "./components/wage/wage-overview/wage-overview.component";
-import { ChartComponent } from "./components/wage/wage-overview/chart/chart.component";
-import { OverviewComponent } from "./components/wage/wage-overview/overview/overview.component";
-import { SearchComponent } from "./components/wage/wage-overview/search/search.component";
-import { DetailComponent } from "./components/wage/wage-overview/detail/detail.component";
-import { HistoryComponent } from "./components/wage/wage-overview/detail/history/history.component";
-import { TotalWageComponent } from "./components/wage/wage-overview/detail/total-wage/total-wage.component";
-import { UserButtonComponent } from "./components/main-nav/sidevar/user-button/user-button.component";
-import { AdminButtonComponent } from "./components/main-nav/sidevar/admin-button/admin-button.component";
-import { IndividualButtonComponent } from "./components/main-nav/sidevar/individual-button/individual-button.component";
-import { ToolbarComponent } from "./components/main-nav/toolbar/toolbar.component";
-import { HomeComponent } from "./components/home/home.component";
-import { HomeInputComponent } from "./components/home/home-input/home-input.component";
-import { HomeFeedComponent } from "./components/home/home-feed/home-feed.component";
-import { SigninComponent } from "./components/signin/signin.component";
-import { LoginBoxComponent } from "./components/signin/login-box/login-box.component";
-import { RegisterComponent } from "./components/register/register.component";
-import { RegisterBoxComponent } from "./components/register/register-box/register-box.component";
-import { StaffComponent } from "./components/user-menus/staff/staff.component";
-import { NewUserComponent } from "./components/main-nav/sidevar/admin-button/new-user/new-user.component";
-import { NoteComponent } from "./components/main-nav/sidevar/user-button/note/note.component";
-import { NoteHistoryComponent } from "./components/main-nav/sidevar/user-button/note/note-history/note-history.component";
-import { NoteInputComponent } from "./components/main-nav/sidevar/user-button/note/note-input/note-input.component";
-import { NoteDatepickerComponent } from "./components/main-nav/sidevar/user-button/note/note-datepicker/note-datepicker.component";
-import { CalendarModule, DateAdapter } from "angular-calendar";
-import { adapterFactory } from "angular-calendar/date-adapters/date-fns";
-import { CalendarComponent } from "./components/main-nav/sidevar/user-button/calendar/calendar.component";
-import { CalendarMainComponent } from "./components/main-nav/sidevar/user-button/calendar/calendar-main/calendar-main.component";
-import { CalendarHeaderComponent } from "./components/main-nav/sidevar/user-button/calendar/calendar-header/calendar-header.component";
-import { CalendarNewScheduleComponent } from "./components/main-nav/sidevar/user-button/calendar/calendar-new-schedule/calendar-new-schedule.component";
-import { MeetingComponent } from "./components/main-nav/sidevar/user-button/calendar/calendar-new-schedule/meeting/meeting.component";
-import { VacationComponent } from "./components/main-nav/sidevar/user-button/calendar/calendar-new-schedule/vacation/vacation.component";
-import { EventComponent } from "./components/main-nav/sidevar/user-button/calendar/calendar-new-schedule/event/event.component";
-import { PermissionComponent } from "./components/main-nav/sidevar/admin-button/permission/permission.component";
-import { PermissionDetailComponent } from "./components/main-nav/sidevar/admin-button/permission/permission-detail/permission-detail.component";
+import { AppComponent } from './app.component';
+import { MainNavComponent } from './components/main-nav/main-nav.component';
+import { SidevarCardComponent } from './components/main-nav/sidevar/sidevar-card/sidevar-card.component';
+import { WageOverviewComponent } from './components/wage/wage-overview/wage-overview.component';
+import { ChartComponent } from './components/wage/wage-overview/chart/chart.component';
+import { OverviewComponent } from './components/wage/wage-overview/overview/overview.component';
+import { SearchComponent } from './components/wage/wage-overview/search/search.component';
+import { DetailComponent } from './components/wage/wage-overview/detail/detail.component';
+import { HistoryComponent } from './components/wage/wage-overview/detail/history/history.component';
+import { TotalWageComponent } from './components/wage/wage-overview/detail/total-wage/total-wage.component';
+import { UserButtonComponent } from './components/main-nav/sidevar/user-button/user-button.component';
+import { AdminButtonComponent } from './components/main-nav/sidevar/admin-button/admin-button.component';
+import { IndividualButtonComponent } from './components/main-nav/sidevar/individual-button/individual-button.component';
+import { ToolbarComponent } from './components/main-nav/toolbar/toolbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { HomeInputComponent } from './components/home/home-input/home-input.component';
+import { HomeFeedComponent } from './components/home/home-feed/home-feed.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { LoginBoxComponent } from './components/signin/login-box/login-box.component';
+import { RegisterComponent } from './components/register/register.component';
+import { RegisterBoxComponent } from './components/register/register-box/register-box.component';
+import { StaffComponent } from './components/user-menus/staff/staff.component';
+import { NewUserComponent } from './components/main-nav/sidevar/admin-button/new-user/new-user.component';
+import { NoteComponent } from './components/main-nav/sidevar/user-button/note/note.component';
+import { NoteHistoryComponent } from './components/main-nav/sidevar/user-button/note/note-history/note-history.component';
+import { NoteInputComponent } from './components/main-nav/sidevar/user-button/note/note-input/note-input.component';
+import { NoteDatepickerComponent } from './components/main-nav/sidevar/user-button/note/note-datepicker/note-datepicker.component';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { CalendarComponent } from './components/main-nav/sidevar/user-button/calendar/calendar.component';
+import { CalendarMainComponent } from './components/main-nav/sidevar/user-button/calendar/calendar-main/calendar-main.component';
+import { CalendarHeaderComponent } from './components/main-nav/sidevar/user-button/calendar/calendar-header/calendar-header.component';
+import { CalendarNewScheduleComponent } from './components/main-nav/sidevar/user-button/calendar/calendar-new-schedule/calendar-new-schedule.component';
+import { MeetingComponent } from './components/main-nav/sidevar/user-button/calendar/calendar-new-schedule/meeting/meeting.component';
+import { VacationComponent } from './components/main-nav/sidevar/user-button/calendar/calendar-new-schedule/vacation/vacation.component';
+import { EventComponent } from './components/main-nav/sidevar/user-button/calendar/calendar-new-schedule/event/event.component';
+import { PermissionComponent } from './components/main-nav/sidevar/admin-button/permission/permission.component';
+import { PermissionDetailComponent } from './components/main-nav/sidevar/admin-button/permission/permission-detail/permission-detail.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { MyInfoComponent } from './components/user-menus/my-info/my-info.component';
 import { WageListOfStaffComponent } from './components/user-menus/wage-list-of-staff/wage-list-of-staff.component';
@@ -94,12 +94,9 @@ import { WageListOfStaffComponent } from './components/user-menus/wage-list-of-s
     EventComponent,
     PermissionComponent,
     PermissionDetailComponent,
-<<<<<<< HEAD
-    PageNotFoundComponent
-=======
+    PageNotFoundComponent,
     MyInfoComponent,
     WageListOfStaffComponent
->>>>>>> 9409547350ca032776359d2557b6f856beddf852
   ],
   imports: [
     BrowserModule,
